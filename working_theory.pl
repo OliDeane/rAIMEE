55a9a2613a2258d9f317723dd9f03a00282ca6a4
OliDeane <oliver.deane@gmail.com>
2023-01-05 22:14:37 +0000

    Edited mutag188 files so that hypothesis can be induced. Fixed intractable compute problem.

--- working_theory.pl
+++ working_theory.pl
@@ -1,42 +1,6 @@
 :-consult("aleph_input/train_aleph.bk").
 
 true_class(A,Ex) :-
-     molecule_logp(A,4.44),
-    Ex = [molecule_logp(A,4.44)].
-
-true_class(A,Ex) :-
-     molecule_inda(A,1),
-    Ex = [molecule_inda(A,1)].
-
-true_class(A,Ex) :-
-     molecule_logp(A,4.23),
-    Ex = [molecule_logp(A,4.23)].
-
-true_class(A,Ex) :-
-     molecule_logp(A,2.29),
-    Ex = [molecule_logp(A,2.29)].
-
-true_class(A,Ex) :-
-     molecule_logp(A,3.26),
-    Ex = [molecule_logp(A,3.26)].
-
-true_class(A,Ex) :-
-     molecule_logp(A,2.52),
-    Ex = [molecule_logp(A,2.52)].
-
-true_class(A,Ex) :-
-     molecule_logp(A,5.87),
-    Ex = [molecule_logp(A,5.87)].
-
-true_class(A,Ex) :-
-     molecule_logp(A,3),
-    Ex = [molecule_logp(A,3)].
-
-true_class(A,Ex) :-
-     molecule_logp(A,4.18),
-    Ex = [molecule_logp(A,4.18)].
-
-true_class(A,Ex) :-
-     molecule_logp(A,4.69),
-    Ex = [molecule_logp(A,4.69)].
+     molecule_has_atoms(A,B), atoms_atype(B,27),
+    Ex = [molecule_has_atoms(A,B), atoms_atype(B,27)].
 
